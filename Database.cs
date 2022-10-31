e54c6b2133ba99392a84ff4cf536cb8fbfcb75c1
hbsxcjp <hbsxcjp@163.com>
2022-10-31 22:18:19 +0800

    GeekPro push.

--- Database.cs
+++ Database.cs
@@ -17,22 +17,19 @@ namespace CChess
         public override string ToString()
         {
             string result = "";
-            var infoList = GetInfoListFromXqbase(Client);
+            var infoList = GetInfoListFromXqbase(6, 10);
             foreach(var info in infoList)
-            {
-                result += Utility.GetString(info) + "\n";
-                //InsertInfo(info);
-            }
-            InsertInfoList(infoList);
+                result += Utility.GetString(info.ToList(), str => str, "\n") + "\n";
 
+            InsertInfoList(infoList);
             return result;
         }
 
-        void InsertInfoList(IEnumerable<Dictionary<string, string>> infoList)
+        private void InsertInfoList(IEnumerable<string[]> infoList)
         {
             using var transaction = SqliteConnection.BeginTransaction();
             // 前提条件：infoList中所有info的Keys()全部相同
-            var infoKeys = infoList.First().Keys;
+            string[] infoKeys = manualFieldNames[..xqbaseHtmlFieldCount];
             var command = SqliteConnection.CreateCommand();
             List<string> values = new();
             foreach(var key in infoKeys)
@@ -47,15 +44,16 @@ namespace CChess
             foreach(var info in infoList)
             {
                 int index = 0;
-                foreach(var key in infoKeys)
-                    command.Parameters[index++].Value = info[key];
+                foreach(var value in info)
+                    command.Parameters[index++].Value = value;
 
                 command.ExecuteNonQuery();
             }
 
             transaction.Commit();
         }
-        void InsertInfo(Dictionary<string, string> info)
+
+        private void InsertInfo(Dictionary<string, string> info)
         {
             SqliteCommand command = new(
                 string.Format($"INSERT INTO {manualTableName} ({string.Join(", ", info.Keys)}) " +
@@ -64,35 +62,35 @@ namespace CChess
             command.ExecuteNonQuery();
         }
 
-        void GetInfo(Dictionary<string, string> info, SqliteDataReader reader)
+        private static void GetInfo(Dictionary<string, string> info, SqliteDataReader reader)
         {
             for(int index = 0;index < reader.FieldCount;++index)
                 info[reader.GetName(index)] = reader.GetString(index);
         }
 
-        public IEnumerable<Dictionary<string, string>> GetInfoListFromXqbase(HttpClient client)
+        private IEnumerable<string[]> GetInfoListFromXqbase(int start, int end)
         {
-            void GetInfoXqbase(Dictionary<string, string> info, string htmlString)
+            void GetInfoXqbase(string[] info, string htmlString)
             {
-                //string[] keys = { "TITLE", "EVENT", "DATE", "SITE", "BLACK", "MOVESTR", "RED", "ECCOSN", "ECCONAME", "RESULT" };
-                string[] keys = manualFieldNames[..10];
+                //{ "TITLE", "EVENT", "DATE", "SITE", "BLACK", "MOVESTR", "RED", "ECCOSN", "ECCONAME", "RESULT" };
                 string pattern = @"<title>(.*?)</title>.*?>([^>]+赛[^>]*?)<.*?>(\d+年\d+月(?:\d+日)?)(?: ([^<]*?))?<.*?>黑方 ([^<]*?)<.*?MoveList=(.*?)"".*?>红方 ([^<]*?)<.*?>([A-E]\d{2})\. ([^<]*?)<.*\((.*?)\)</pre>";
                 Regex regex = new(pattern, RegexOptions.Singleline);
                 Match match = regex.Match(htmlString);
                 if(match.Success)
-                    for(int i = 0;i < keys.Length;i++)
-                        info[keys[i]] = i != 5 ? match.Groups[i + 1].Value : match.Groups[i + 1].Value.Replace("-", "");
+                    for(int i = 1;i < info.Length;i++)
+                        info[i] = i != 6 ? match.Groups[i].Value : match.Groups[i].Value.Replace("-", "");
             }
 
             Encoding codec = Encoding.GetEncoding("gb2312");
-            Task<Dictionary<string, string>>[] taskArray = new Task<Dictionary<string, string>>[5]; //总数:12141
+            Task<string[]>[] taskArray = new Task<string[]>[end - start + 1]; //总数:12141
             for(int i = 0;i < taskArray.Length;i++)
             {
-                string uri = string.Format(@"https://www.xqbase.com/xqbase/?gameid={0}", i + 1);
-                taskArray[i] = Task<Dictionary<string, string>>.Factory.StartNew(() =>
+                string uri = string.Format(@"https://www.xqbase.com/xqbase/?gameid={0}", i + start);
+                taskArray[i] = Task<string[]>.Factory.StartNew(() =>
                 {
-                    Dictionary<string, string> info = new() { { "SOURCE", uri } };
-                    var taskA = client.GetByteArrayAsync(uri);
+                    string[] info = new string[xqbaseHtmlFieldCount];
+                    info[0] = uri;
+                    var taskA = Client.GetByteArrayAsync(uri);
                     GetInfoXqbase(info, codec.GetString(taskA.Result));
                     return info;
                 });
@@ -100,6 +98,15 @@ namespace CChess
 
             Task.WaitAll(taskArray);
             return taskArray.Select(task => task.Result);
+        }
+
+        private void InitDatabase()
+        {
+            SqliteCommand command = new(string.Format(
+                $"CREATE TABLE {manualTableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                $"{string.Join(",", manualFieldNames.Select(field => field + " TEXT"))})"),
+                SqliteConnection);
+            command.ExecuteNonQuery();
         }
 
         private SqliteConnection SqliteConnection
@@ -123,20 +130,12 @@ namespace CChess
         }
         private HttpClient Client { get { return _client ??= new(); } }
 
-        private void InitDatabase()
-        {
-            SqliteCommand command = new(string.Format(
-                $"CREATE TABLE {manualTableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                $"{string.Join(",", manualFieldNames.Select(field => field + " TEXT"))})"),
-                SqliteConnection);
-            command.ExecuteNonQuery();
-        }
-
         private readonly string databaseFileName = "data.db";
         private readonly string manualTableName = "manual";
         private readonly string[] manualFieldNames = {
-            "TITLE", "EVENT", "DATE", "SITE", "BLACK", "MOVESTR", "RED", "ECCOSN", "ECCONAME", "RESULT",
-            "OPENING", "WRITER", "AUTHOR", "TYPE", "VERSION", "FEN", "ROWCOLS", "SOURCE", "CALUATE_ECCOSN" };
+            "SOURCE", "TITLE", "EVENT", "DATE", "SITE", "BLACK", "MOVESTR", "RED", "ECCOSN", "ECCONAME", "RESULT",
+            "OPENING", "WRITER", "AUTHOR", "TYPE", "VERSION", "FEN", "ROWCOLS", "CALUATE_ECCOSN" };
+        private readonly int xqbaseHtmlFieldCount = 11;
 
         private SqliteConnection? _connection;
         private HttpClient? _client = null;
